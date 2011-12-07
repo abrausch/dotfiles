e0acff260d1ca1c0e1384a0e7ef1fb7aee84dd38
Alexander Brausch <alexander.brausch@caperwhite.com>
2011-12-07 11:42:45 +0100

    Change some vim options, add DS_Store to gitignore

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,10 @@
 export PATH=".:bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH:/Applications/TextMate.app/Contents/Resources/"
+export PATH=/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/X11/bin:/opt/local/bin
+export PATH=/usr/local/bin:/usr/local/scala/bin:/opt/local/bin:/opt/local/sbin:$PATH:/Applications/Development/products/apache-maven-2.0.9/bin:/Applications/TextMate.app/Contents/Resources:/usr/local/mysql/bin:~/bin
+export PATH=$PATH:/Users/abrausch/Projekte/Caperwhite/git/Xcode/bin:/Users/abrausch/Projekte/Caperwhite/trunk/MetaTool:/Users/abrausch/Projekte/Caperwhite/git/Xcode/Scripts:/Users/abrausch/.gem/ruby/1.8/bin/:/opt/go/bin
+export PATH=$PATH:~/local/bin:/usr/local/sbin
+export JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/1.6/Home
+export SCALA_HOME=/usr/local/scala/
+export PATH=$PATH:/Applications/Inkscape.app/Contents/Resources/bin
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
