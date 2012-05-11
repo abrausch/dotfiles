71463c286a287418bb27f5a2975557eba103b8d7
Alexander Brausch <alexander.brausch@caperwhite.com>
2012-05-11 10:19:31 +0200

    Bundles vimrc, error in zsh aliases

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,11 +1,13 @@
 # grc overides for ls
 #   Made possible through contributions from generous benefactors like
 #   `brew install coreutils`
-if $(gls &>/dev/null)
+if $(/usr/local/bin/gls &>/dev/null)
 then
   alias ls="gls -F --color"
   alias l="gls -lAh --color"
   alias ll="gls -l --color"
   alias la='gls -A --color'
   alias emacs="/usr/local/Cellar/emacs/HEAD/Emacs.app/Contents/MacOS/Emacs -nw"
+else
+	echo "No gls"
 fi
