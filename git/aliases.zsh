ed95d18de72c899fef93ab0a8609b8e5c5275bde
Alexander Brausch <alexander.brausch@caperwhite.com>
2013-03-12 15:25:29 +0100

    Add some usefull aliases to git

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -4,7 +4,8 @@ alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C
 alias gp='git push origin HEAD'
 alias gd='git diff'
 alias gc='git commit'
-alias gca='git commit -a'
+alias gca='git commit -v -a'
+alias gcv='git commit -v'
 alias gco='git checkout'
 alias gb='git branch'
 alias gst='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
