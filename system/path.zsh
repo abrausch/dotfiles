0eaf50d547e4e93a8aa6e7dc0fc5e05ef6349853
Alexander Brausch <alexander.brausch@caperwhite.com>
2014-07-03 14:20:51 +0200

    Changes

--- system/path.zsh
+++ system/path.zsh
@@ -1,13 +1,14 @@
 export PATH=".:bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH:/Applications/TextMate.app/Contents/Resources/"
 export PATH=/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/X11/bin:/opt/local/bin
 export PATH=/usr/local/bin:/usr/local/scala/bin:/opt/local/bin:/opt/local/sbin:$PATH:/Applications/Development/products/apache-maven-2.0.9/bin:/Applications/TextMate.app/Contents/Resources:/usr/local/mysql/bin:~/bin
-export PATH=$PATH:/Users/abrausch/Projekte/Caperwhite/git/Xcode/bin:/Users/abrausch/Projekte/Caperwhite/trunk/MetaTool:/Users/abrausch/Projekte/Caperwhite/git/Xcode/Scripts:/Users/abrausch/.gem/ruby/1.8/bin/:/opt/go/bin
+export PATH=$PATH:/Users/abrausch/Projekte/Caperwhite/git/Xcode/bin:/Users/abrausch/Projekte/Caperwhite/git/MetaTool/target/release:/Users/abrausch/Projekte/Caperwhite/git/Xcode/Scripts:/Users/abrausch/.gem/ruby/1.8/bin/:/opt/go/bin
 export PATH=$PATH:~/local/bin:/usr/local/sbin
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.7.0_07.jdk/Contents/Home
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.7.0_45.jdk/Contents/Home
 export SCALA_HOME=/usr/local/scala/
 export PATH=$PATH:/Applications/Inkscape.app/Contents/Resources/bin
 export PATH=/Users/abrausch/Projekte/DirtyLittleHelper:$PATH
 export PATH=$PATH:~/bin/oclint-0.7/bin
 export PATH=$HOME/.cask/bin:$PATH
-
+export PATH=$PATH:~/bin/oclint/bin
+export PATH=$HOME/.homebrew/homebrew/bin:$PATH
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
